4c63383eaaec8336b70f1985b92b88249a8c5247
phanimonish <phanimonish9261@gmail.com>
2024-01-19 09:11:39 +0530

    devices

--- src/Components/Card/Card.jsx
+++ src/Components/Card/Card.jsx
@@ -13,6 +13,14 @@ const responsive = {
     breakpoint: { max: 3000, min: 1024 },
     items: 1,
   },
+  tablet: {
+    breakpoint: { max: 1024, min: 464 },
+    items: 1
+  },
+  mobile: {
+    breakpoint: { max: 464, min: 0 },
+    items: 1
+  }
 };
 function Card() {
   return (
